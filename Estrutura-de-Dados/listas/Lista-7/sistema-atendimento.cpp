2411241959df4ce78f4b576b884695fa71690992
Diogo <ggdiogogg@gmail.com>
2024-05-15 23:11:15 -0300

    feat: lista-7 sistema de atendimento

--- Estrutura-de-Dados/listas/Lista-7/sistema-atendimento.cpp
+++ Estrutura-de-Dados/listas/Lista-7/sistema-atendimento.cpp
@@ -4,74 +4,82 @@
 
 using namespace std;
 
-int inserirCliente();
-int atenderCliente();
-int mostrarFila();
-int encerrarSistema();
+int inserirCliente(queue<string> &fila);
+int atenderCliente(queue<string> &fila);
+int mostrarFila(queue<string> fila);
+int encerrarSistema(bool &continuar);
 
-int main(){
+int main() {
+    setlocale(LC_ALL, "Portuguese");
 
     int codigo;
     bool continuar = true;
     string cliente;
     queue<string> fila;
 
-    while(continuar == true){
+    while (continuar) {
+        cout << "1 - Adicionar novo cliente à fila" << endl;
+        cout << "2 - Atender cliente" << endl;
+        cout << "3 - Mostrar a fila" << endl;
+        cout << "4 - Encerrar programa" << endl;
 
-        cout<<"1 - Adicionar novo cliente a fila"<<endl;
-        cout<<"2 - Atender cliente"<<endl;
-        cout<<"3 - Mostrar a fila"<<endl;
-        cout<<"4 - Encerrar programa"<<endl;
+        cout << "Escolha uma opção: ";
+        cin >> codigo;
 
-        switch(codigo){
-
+        switch (codigo) {
             case 1:
-                inserirCliente();
-            break;
+                inserirCliente(fila);
+                break;
             case 2:
-                atenderCliente();
-            break;
+                atenderCliente(fila);
+                break;
             case 3:
-                mostrarFila();
-            break;
+                mostrarFila(fila);
+                break;
             case 4:
-                encerrarSistema();
-            break;
+                encerrarSistema(continuar);
+                break;
+            default:
+                cout << "Opção inválida!" << endl;
+                break;
         }
-
     }
+
+    return 0;
 }
 
-int inserirCliente(){
-
-    cout<<"Digite o nome do cliente: "<<endl;
+int inserirCliente(queue<string> &fila) {
+    string cliente;
+    cout << "Digite o nome do cliente: ";
+    cin.ignore();
     getline(cin, cliente);
-
     fila.push(cliente);
-
+    return 0;
 }
 
-int atenderCliente(){
-
-    fila.pop();
-
-}
-
-int mostrarFila(){
-
-    queue<string>filaAuxiliar = fila;
-
-    cout<<"Clientes na fila: ";
-    while(!filaAuxiliar.empty()){
-
-        cout<<filaAuxiliar.front()<<" ";
+int atenderCliente(queue<string> &fila) {
+    if (!fila.empty()) {
+        cout << "Cliente atendido: " << fila.front() << endl;
         fila.pop();
+    } else {
+        cout << "Não há clientes na fila!" << endl;
     }
-    cout<<endl;
+    return 0;
 }
 
-int encerrarSistema(){
+int mostrarFila(queue<string> fila) {
+    queue<string> filaAuxiliar = fila;
 
+    cout << "Clientes na fila: ";
+    while (!filaAuxiliar.empty()) {
+        cout << filaAuxiliar.front() << " ";
+        filaAuxiliar.pop();
+    }
+    cout << endl;
+    return 0;
+}
+
+int encerrarSistema(bool &continuar) {
     continuar = false;
-
+    return 0;
 }
